084307ac9488965f9d0e06c58142d31a6ed7ecb5
Varek <4ndr3r13ck@googlemail.com>
2013-06-02 12:41:30 +0200

    tweak demo 2

--- db/migrate/20130601134859_add_dummy_coupon_data.rb
+++ db/migrate/20130601134859_add_dummy_coupon_data.rb
@@ -2,7 +2,7 @@ class AddDummyCouponData < ActiveRecord::Migration
   def up
     @cp = CouponProvider.create(:name => 'Foobar', :url => 'foobar.com')
     coupon_code = SecureRandom.hex.to_s[0..5]
-    Coupon.create(:coupon_provider_id => @cp.id, :code => coupon_code, :coupon_type => "#{SecureRandom.random_number(14)+1}% discount", photo_id: 13873789, username: 'spiegeleule', user_id: 160004, printed: true)
+    Coupon.create(:coupon_provider_id => @cp.id, :code => coupon_code, :coupon_type => "#{SecureRandom.random_number(14)+1}% discount", photo_id: 13873789, username: 'spiegeleule', user_id: 160004, printed: true, updated:"2013-06-02T12:29:46+0200")
     coupon_code = SecureRandom.hex.to_s[0..5]
     Coupon.create(:coupon_provider_id => @cp.id, :code => coupon_code, :coupon_type => "#{SecureRandom.random_number(14)+1}% discount")
     # 10.times do |i|
